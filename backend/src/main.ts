e661d3b132fd4a58549430c70a0a7b6651c0934d
Suraj <spsuraj2004@gmail.com>
2026-06-05 12:19:50 +0530

    gg

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://tutoring-platform-kcl0kw805-spsuraj2004s-projects.vercel.app',
+    ],
     credentials: true,
   });
 
